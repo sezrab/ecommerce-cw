52bde309e08f7cb8b5b033922b90fe427ccc8c4c
samuelezraberry <samuel.ezra.berry@gmail.com>
2023-05-03 19:16:26 +0100

    Basket Working

--- shop.py
+++ shop.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
 # flask bootstrap
 from flask_bootstrap import Bootstrap
 # flaskwtf form
 from flask_wtf import FlaskForm as Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Length, DataRequired
-
-
-# get sql alchemy from flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = ['C1B6-1F3C-4F1A-8F9C']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shopdb.sqlite3'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
@@ -36,13 +34,63 @@ class Item(db.Model):
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', items=Item.query.all())
+    return render_template('index.html', items=Item.query.all(), basket_count=len(session.get('basket', [])))
 
 
-@app.route('/view', methods=['GET'])
+@app.route('/basket', methods=['GET'])
+def basket():
+    item_ids = session.get('basket', [])
+    # items = Item.query.filter(Item.id.in_(item_ids)).all()
+    items = {}
+    for id in item_ids:
+        itm = Item.query.get(id)
+        if itm not in items:
+            items[itm] = 1
+        else:
+            items[itm] = items[itm] + 1
+    total = 0
+    for item in items:
+        count = items[item]
+        total += item.price * count
+    return render_template('basket.html', items=items, basket_count=len(session.get('basket', [])), total=total)
+
+
+@ app.route('/add', methods=['POST'])
+def add():
+    # add to basket
+    if 'basket' not in session:
+        session['basket'] = []
+    itmid = request.form.get('id')
+    print(itmid)
+    session['basket'] = session['basket'] + [itmid]
+    print(session['basket'])
+    return redirect(url_for('index'))
+
+
+@app.route('/remove', methods=['POST'])
+def remove():
+    # remove from basket
+    if 'basket' not in session:
+        session['basket'] = []
+    itmid = request.form.get('id')
+
+    # REMOVE FIRST OCCURENCE ONLY OF ITMID FROM BASKET
+    basket = session['basket']
+    idx = basket.index(itmid)
+    basket.pop(idx)
+    session['basket'] = basket
+    return redirect(url_for('basket'))
+
+
+@ app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html', basket_count=len(session.get('basket', [])))
+
+
+@ app.route('/view', methods=['GET'])
 def view():
     itemid = request.args.get('id')
-    return render_template('product.html', item=Item.query.get(itemid))
+    return render_template('product.html', item=Item.query.get(itemid), basket_count=len(session.get('basket', [])))
 
 
 if __name__ == '__main__':
